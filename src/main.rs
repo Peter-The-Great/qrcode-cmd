2fa6aca28862938ccc46a4bfa4a3fe32aa4fe5a2
Peter-The-Great <PjotrW15@Gmail.com>
2023-06-02 21:29:55 +0200

    created an commandline interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use qrcode::QrCode;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Get the command-line argument for the input text
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Please provide the input text as an argument.");
+        return;
+    }
+    let input_text = &args[1];
+
+    // Generate the QR code
+    let code = QrCode::new(input_text).expect("Failed to generate QR code.");
+
+    // Render the QR code as a SVG file
+    let string = code.render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+    println!("{}", string);
 }
